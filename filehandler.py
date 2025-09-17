7306345df4a4bc306b41e0bf75ccdcdf2b164829
AltoidDev <100539243+AltoidDev@users.noreply.github.com>
2025-09-17 09:22:24 -0700

    fixed me forgetting this wasn't gdscript

--- filehandler.py
+++ filehandler.py
@@ -1,6 +1,6 @@
 import json
 
-def encode(filetext:String):
+def encode(filetext:str):
     try:
         conf = json.loads(filetext)
     except:
@@ -9,36 +9,34 @@ def encode(filetext:String):
     level = 0
 
     buffer:str=""
-
-    for x in conf.keys():
-        y = conf[x]
-        if type(y) != dict:
-            buffer = buffer + f"*{x}\n\t{y}"
-
-    def search(object,recusion:int=0):
-        if recusion > 0:
+    if type(conf) == dict:
+        for x in conf.keys():
+            y = conf[x]
+            if type(y) != dict:
+                buffer = buffer + f"*{x}\n\t{y}"
+    else:
+        print("Config file isn't valid json syntax, please fix it")
+    def search(object,recursion:int=0):
+        if recursion > 0:
             print(f"Checking recursive dict at level {recursion} with value {object}")
 
         match object:
-            case str:
+            case str():
                 print("Type: header")
-                if object.begins_with("#l"):
+                if object.startswith("#l"):
                     pass #handle the literals dumb bass (fih)
 
-            case list:
+            case list():
                 print("Type: list")
                 if object[0] == "#l":
                     pass #handle the literals dumb bass (fih)
 
-            case dict:
+            case dict():
                 print("Type: entry")
                 for x in object.keys():
                     y = object[x]
-                    search(y,recusion+1)
+                    search(y,recursion+1)
 
             case _:
-                print(f"unknown element {x}")
-                
-        if is_recursive:
-            pass
+                print(f"unknown element {object}")
     return(buffer)
--- main.py
+++ main.py
@@ -5,16 +5,15 @@ import filehandler
 working = os.getcwd()
 cpath = working+"config.json"
 
-if not os.is_file(f"{working}Carl.jpg"):
+if not os.path.exists(f"{working}Carl.jpg"):
     sys.exit()
 
-def read_config(filename:String):
-    if os.is_file(f"{working}config.json"):
+def read_config():
+    if os.path.exists(f"{working}config.json"):
         config = open(cpath,'r')
         print(config)
-        config = filehandler.encode(config)
+        config = filehandler.encode(config.read())
         with open(working+"config.org","W") as file:
             file.write(config)
-        config.close()
     else:
         sys.exit()
